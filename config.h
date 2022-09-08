ff7f2f759ce82946db7aab526e154846fd0f94ae
ahmadhassandebugs <ahmadhassansandhu@gmail.com>
2022-09-08 21:03:15 +0000

    fix: increase msg timeout to 15 sec

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 
 const uint64_t PKT_PAYLOAD_LEN = 1200; // in bytes
 const uint64_t RECV_BUFFER_LEN = 65536; // in bytes
-const uint16_t SERVER_RECV_MSG_TIMEOUT = 2; // in secs
+const uint16_t SERVER_RECV_MSG_TIMEOUT = 15; // in secs
 const double BITS_PER_BYTE = 8.0;
 const double KILO = 1024.0;
 const double MEGA = KILO * KILO;
